828d75192117b7c422700c9d8c3e0d4a95c5cbee
retardedparth <parthkushwah6375@gmail.com>
2024-12-01 03:24:29 +0530

    Update Scroller.jsx

--- src/Scroller.jsx
+++ src/Scroller.jsx
@@ -2,7 +2,7 @@ import "./Scroller.css";
 
 const Scroller = () => {
   const message =
-    "Breaking News:- The wonderful Pink Life Vol.2 Out now! : Parth ne firse saabit kiya woh hai retarded. : Pink-Lifers ke beech faili nai bimaari. Naam baya jaa raha hai Deadlock. : Nahi subside hue Valorant outbreak ke symptoms, dozen logon ne kiya rage aur seethe. : Arew3y ne bola 'आत्मधुन तर आपन goth baddie.' to the surprise of no one. :";
+    "Breaking News:- The wonderful Pink Life Vol.2 Out now! : Parth ne firse saabit kiya woh hai bhagwan. : Pink-Lifers ke beech faili nai bimaari. Naam baya jaa raha hai Deadlock. : Nahi subside hue Valorant outbreak ke symptoms, dozen logon ne kiya rage aur seethe. : Arew3y ne bola 'आत्मधुन तर आपन goth baddie.' to the surprise of no one. :";
 
   return (
     <div className="scroller-container">
