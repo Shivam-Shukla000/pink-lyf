c0eddfae713b0690216d8468ab9d3a0fcc6ac504
shivam <shuklashivamdeos@gmail.com>
2025-02-09 09:59:17 +0530

    password protected videos

--- src/views/Video.jsx
+++ src/views/Video.jsx
@@ -1,10 +1,72 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 export default function Video() {
   const link1 = "https://www.youtube.com/embed/R0E5Q8AaXMQ";
+  const password = "parth";
+  const [displayState1, setDisplatState1] = useState("");
+  const [displayState2, setDisplatState2] = useState("");
+  const [message, setMessage] = useState("");
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    setDisplatState1("flex");
+    setDisplatState2("none");
+  }, []);
+
+  function handleValidate() {
+    if (input == password) {
+      setDisplatState1("none");
+      setDisplatState2("flex");
+    } else {
+      setMessage("invalid password");
+    }
+  }
 
   return (
     <>
-      <Flex justifyContent={"center"} alignContent={"center"} marginTop={"3%"}>
+      <Flex
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignContent={"center"}
+        display={displayState1}
+        // width={"100%"}
+        height={"100%"}
+      >
+        <Input
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          margin={"0"}
+          marginLeft={"auto"}
+          marginRight={"auto"}
+          value={input}
+          placeholder="Enter Password"
+          maxWidth={"300px"}
+          bg={"white"}
+        ></Input>
+        <Box>{message}</Box>
+        <Button
+          boxSize={"fit-content"}
+          bg={"#F46B2F"}
+          marginLeft={"auto"}
+          marginRight={"auto"}
+          border={"1px solid black"}
+          padding={"10px 15px 10px 15px"}
+          maxWidth={"200px"}
+          fontSize={"20px"}
+          textColor={"white"}
+          _hover={{ bg: "black" }}
+          onClick={handleValidate}
+        >
+          Validate
+        </Button>
+      </Flex>
+      <Flex
+        marginTop={"3%"}
+        display={displayState2}
+        justifyContent={"center"}
+        alignContent={"center"}
+      >
         <iframe
           width="1273"
           height="716"
